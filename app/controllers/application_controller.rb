9156282b1e79623b4068e064ad97c6149534db34
yota.hamasaki <journey231517@yahoo.co.jp>
2021-03-06 04:07:24 +0000

    post

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,3 +10,15 @@ class ApplicationController < ActionController::Base
         end
     end
 end
+
+  private
+
+  def require_user_logged_in
+    unless logged_in?
+      redirect_to login_url
+    end
+  end
+
+  def counts(user)
+    @count_microposts = user.microposts.count
+  end
